20fb83ab2610083a1cdb32539e52c27acd8f6052
lenth <truocluong@gmail.com>
2019-04-23 13:06:28 +0700

    Assiment2

---
+++ source/repos/Assigment2/Assigment2/Cart.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assigment2
+{
+    class Cart
+    {
+        int id;
+        String customer;
+        double grandTotal;
+        List<Product> productList;
+        String coutry;
+        String city;
+
+        public Cart()
+        {
+        }
+
+        public Cart(int id, string customer, double grandTotal, List<Product> productList, string coutry, string country)
+        {
+            this.id = id;
+            this.customer = customer;
+            this.grandTotal = grandTotal;
+            this.productList = productList;
+            this.coutry = coutry;
+            this.country = country;
+        }
+        public void AddProduct(Product product)
+        {
+            productList.Add(product);
+        }
+        public void removeProduct(int index)
+        {
+            productList.RemoveAt(index);
+        }
+        public double checkgrandTotal(){
+            if(this.city.Equals("Hà Nội")){
+            return    this.grandTotal= grandTotal / 100 *1 ;
+}else if(!this.coutry.Equals("Việt Nam")){
+              return  this.grandTotal= grandTotal / 100 *5 ;
+
+}
+          else {
+                return this.grandTotal= grandTotal / 100 *5;
+}
+
+
+}
+
+
+}
+
+    }
+}
---
+++ source/repos/Assigment2/Assigment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assigment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+           List<String>list = new List<string>();
+            list.add('1');
+           Product produc1 = new Product(1,"IphoneX",123232,2,"image1","Day la iphoneX","Test",list,1);
+            //ADd spam
+            Cart cart = new Cart();
+            //
+            cart.AddProduct(produc1);
+                cart.removeProduct(1);
+
+
+        }
+    }
+}
